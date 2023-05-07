963b936072e6e5aa23abbc3aa76aa06da6beaa9e
Amina S <aminchik2809@mail.ru>
2023-05-07 19:54:39 +0300

    add files

---
+++ Data Access/ConstantsForConnectionToDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSixteenthProgram
+{
+    internal static class ConstantsForConnectionToDB
+    {
+        public const string Login = "postgres";
+        public const string Password = "postgres";
+        public const string Host = "localhost";
+        public const string Port = "5432";
+        public const string DbName = "Shop";
+
+        public static string ConnectionString { get; } = $"User ID={Login};Password={Password};Host={Host};Port={Port};Database={DbName};";
+
+    }
+}
